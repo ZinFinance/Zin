4af27865f715c6d4233dd02c7961194973001dce
Ovais Hanif <ovaishanif94@gmail.com>
2020-08-06 22:38:09 +0500

    Add migration for tfa code alter user table

---
+++ Zin/Migrations/20200806173334_addTfaColumnUserTable.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Zin.Migrations
+{
+    public partial class addTfaColumnUserTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TfaCode",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TfaCode",
+                table: "AspNetUsers");
+        }
+    }
+}
